416e2015b3a472a8ebeee97e82a10934c54fc18e
Phin-The-Human <phintineke@gmail.com>
2024-12-22 19:16:43 +0100

    added IfStatements

---
+++ IfStatements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+number=10
+#simple if statement
+if [ $number -eq 10 ]; then #if the number is eual to 10
+    echo "The number is 10."
+fi
+
+number=5
+# if else statement
+if [ $number -eq 10 ]; then
+    echo "The number is 10"
+else
+    echo "The number is not 10"
+fi
