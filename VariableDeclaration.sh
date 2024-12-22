687ce152599670953bb8d3fc5a8d219f33434808
Phin-The-Human <phintineke@gmail.com>
2024-12-22 19:21:04 +0100

    Modified line in variable declaration

--- VariableDeclaration.sh
+++ VariableDeclaration.sh
@@ -2,5 +2,5 @@
 name=jhn
 marks=90
 
-echo The name of the candidate is $name and they got $marks marks #Output would be 90
+echo "The name of the candidate is $name and they got $marks marks" #Output would be 90
 
